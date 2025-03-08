d3a73857aa0bb33c58174d70c7726110b870e670
Samuel Dushime <sdushime38@gmail.com>
2025-03-08 14:43:45 +0200

    d

--- Frontend/app/Components/Navbar.tsx
+++ Frontend/app/Components/Navbar.tsx
@@ -22,21 +22,22 @@ const Navbar: React.FC = () => {
         isScrolled ? ' backdrop-blur-md' : 'bg-transparent'
       }`}
     >
-      <div className="max-w-7xl  flex  items-center p-4">
-        <div className="text-white text-xl rounded-full border border-gray-600">
+      <div className="max-w-7xl  flex  items-center p-4 gap-5">
+        <div className="text-white text-xl rounded-full border border-gray-500">
           <Link href="/" className="">
             <img
-              src="/file.png"
+              src="/logo.png"
               alt="LeafSense Logo"
-              className="max-w-60 h-auto -my-14 -mx-8"
+              className="max-w-60 h-auto -my-6"
             />
           </Link>
         </div>
-        <div className="space-x-8 text-white rounded-full border border-gray-600 px-4 py-2 text-md">
-          <Link href="/scan">Scan Plant</Link>
-          <Link href="/library">Plant Library</Link>
-          <Link href="/results">History</Link>
-          <Link href="/about">About</Link>
+        <div className="space-x-8 text-white rounded-full border border-gray-500 px-4 py-2 text-md">
+          <Link href="/">Home</Link>
+          <Link href="/detect">Detect Disease</Link>
+          <Link href="/diseases">Disease Database</Link>
+          <Link href="/about">About Us</Link>
+          <Link href="/contact">Contact</Link>
         </div>
       </div>
     </nav>
